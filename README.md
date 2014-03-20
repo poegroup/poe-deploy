21ceef6032ed02f157b9c028af61a7357358a439
CamShaft <bytheway.cameron@gmail.com>
2014-03-20 00:44:17 -0600

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ poe-deploy
 ==========
 
 A simple deploy application with GitHub and Heroku integration
+
+Usage
+-----
+
+```js
+// app.js
+var deploy = require('poe-deploy');
+
+var app = module.exports = deploy();
+```
+
+The exported app can now be run with [startup](https://github.com/camshaft/startup.git).
+
+```sh
+$ startup start -p app.js
+```
+
+Environment Variables
+---------------------
+
+`GITHUB_TOKEN` - GitHub access token for cloning the repo
+`GITHUB_SECRET` - A secret token shared between GitHub and the `poe-deploy` instance to verify webhooks
+`HEROKU_TOKEN` - Heroku access token for deploying and creating a apps
+`HEROKU_PREFIX` - A prefix to use when deploying to heroku, i.e. `poe` would result in `poe-myapp-prod`
---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies
+ */
+
+var grappler = require('grappler');
+var envs = require('envs');
+var github = require('grappler-hook-github');
+var heroku = require('grappler-deploy-heroku');
+var stdout = require('grappler-logger-stdout');
+
+/**
+ * Create a poe-deploy app
+ */
+
+module.exports = function(opts) {
+  opts = opts || {};
+  opts.github = opts.github || {};
+  opts.heroku = opts.heroku || {};
+
+  var app = grappler();
+
+  app.plugin(github({
+    secret: envs('GITHUB_SECRET', opts.github.secret),
+    token: envs('GITHUB_TOKEN', opts.github.token)
+  }));
+
+  app.plugin(heroku({
+    prefix: envs('HEROKU_PREFIX', opts.heroku.prefix),
+    token: envs('HEROKU_TOKEN', opts.heroku.token)
+  }));
+
+  app.plugin(stdout());
+
+  return app;
+};
