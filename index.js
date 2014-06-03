3bbed4e19223ee646c6aaf918e751ab19eadebb9
camshaft <bytheway.cameron@gmail.com>
2014-06-03 03:30:45 -0600

    add task config

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var envs = require('envs');
 var github = require('grappler-hook-github');
 var heroku = require('grappler-deploy-heroku');
 var stdout = require('grappler-logger-stdout');
+var config = require('grappler-task-config');
 
 /**
  * Create a poe-deploy app
@@ -20,6 +21,12 @@ module.exports = function(opts) {
   var app = grappler();
 
   var GITHUB_TOKEN = envs('GITHUB_TOKEN', opts.github.token);
+
+  app.on('task', function(task) {
+    task.use(config({
+      githubToken: GITHUB_TOKEN
+    }));
+  });
 
   app.plugin(github({
     secret: envs('GITHUB_SECRET', opts.github.secret),
